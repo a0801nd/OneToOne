58f32ea43a1cc4996ae73475315bb8f1f4079a50
an4lexander <an4lexander@gmail.com>
2023-02-09 20:04:32 +0200

    Removed CLTypingLabel pod and added the same method with cycle

--- OneToOne/Controllers/MainVC.swift
+++ OneToOne/Controllers/MainVC.swift
@@ -1,19 +1,26 @@
 import UIKit
-import CLTypingLabel
 
 class MainVC: UIViewController {
     // MARK: - IBOutlets
-    @IBOutlet weak var logoLabel: CLTypingLabel!
     
-    // MARK: - Objects
+    @IBOutlet weak var logoLabel: UILabel!
     
     // MARK: - ViewDidLoad
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        logoLabel.text = "<-OneToOne:->"
+        logoLabel.text = ""
+        var charIndex = 0.0
+        for letter in K.appName {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+                self.logoLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
     }
     
     // MARK: - IBActions
+    
     @IBAction func registerButtonPressed(_ sender: UIButton) {}
     @IBAction func loginButtonPressed(_ sender: UIButton) {}
 }
