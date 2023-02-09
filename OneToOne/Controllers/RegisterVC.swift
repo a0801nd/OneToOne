02fb53e3bb88dd97c686e20dbeac6974e9022254
an4lexander <an4lexander@gmail.com>
2023-02-09 20:09:10 +0200

    Added a method that gives access to the button depending on the number of password characters and added extension to UITextField

--- OneToOne/Controllers/RegisterVC.swift
+++ OneToOne/Controllers/RegisterVC.swift
@@ -4,18 +4,25 @@ import FirebaseFirestore
 import FirebaseAuth
 
 class RegisterVC: UIViewController {
+    
     // MARK: - IBOutlets
     @IBOutlet weak var emailTF: UITextField!
     @IBOutlet weak var passwordTF: UITextField!
-    // MARK: - Objects
+    @IBOutlet weak var registerButton: UIButton!
     
+    // MARK: - Objects
+    var delegate: UITextFieldDelegate?
     
     // MARK: - ViewDidLoad
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        emailTF.delegate = self
+        passwordTF.delegate = self
     }
     
     // MARK: - IBActions
+    
     @IBAction func registerButtonPressed(_ sender: UIButton) {
         if let email = emailTF.text, let password = passwordTF.text {
             Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
@@ -27,5 +34,22 @@ class RegisterVC: UIViewController {
                 }
             }
         }
+    }
+    @IBAction func passwordTextFieldChanged(_ sender: UITextField) {
+        if sender.text!.count < 6 {
+            registerButton.layer.backgroundColor = #colorLiteral(red: 1, green: 0.3921568627, blue: 0.3921568627, alpha: 0.8964710884)
+            registerButton.isEnabled = false
+        } else {
+            registerButton.layer.backgroundColor = #colorLiteral(red: 0.337254902, green: 0.4431372549, blue: 0.537254902, alpha: 0.7983365221)
+            registerButton.isEnabled = true
+        }
+    }
+}
+// MARK: - Extensions
+
+extension RegisterVC: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.endEditing(true)
+        return true
     }
 }
