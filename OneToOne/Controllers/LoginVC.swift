cab15b71e1ff68bf1897feb173849df5626cab25
an4lexander <an4lexander@gmail.com>
2023-02-09 19:57:44 +0200

    Added a method that gives access to the button depending on the number of password characters

--- OneToOne/Controllers/LoginVC.swift
+++ OneToOne/Controllers/LoginVC.swift
@@ -5,17 +5,33 @@ import FirebaseAuth
 
 class LoginVC: UIViewController {
     // MARK: - IBOutlets
+    
     @IBOutlet weak var emailTF: UITextField!
     @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
     
     // MARK: - Objects
+    var delegate: UITextFieldDelegate?
     
     // MARK: - ViewDidLoad
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        emailTF.delegate = self
+        passwordTF.delegate = self
     }
     
     // MARK: - IBActions
+    
+    @IBAction func passwordTextFieldChanged(_ sender: UITextField) {
+        if sender.text!.count < 6 {
+            loginButton.layer.backgroundColor = #colorLiteral(red: 1, green: 0.3921568627, blue: 0.3921568627, alpha: 0.8964710884)
+            loginButton.isEnabled = false
+        } else {
+            loginButton.layer.backgroundColor = #colorLiteral(red: 0.337254902, green: 0.4431372549, blue: 0.537254902, alpha: 0.7983365221)
+            loginButton.isEnabled = true
+        }
+    }
     @IBAction func loginButtonPressed(_ sender: UIButton) {
         if let email = emailTF.text, let password = passwordTF.text {
             Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
@@ -26,5 +42,13 @@ class LoginVC: UIViewController {
                 }
             }
         }
+    }
+}
+// MARK: - Extensions
+
+extension LoginVC: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.endEditing(true)
+        return true
     }
 }
